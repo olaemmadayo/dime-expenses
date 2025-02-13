2df4d8db04faa238504fb6fa826e7b8c85f5296d
omodaraodubiro <oiretunde@gmail.com>
2025-02-13 14:12:11 +0100

    pricing section and local storage

---
+++ localStorage/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TransactionsList from "./TransactionsList";
+
+function App() {
+  return (
+    <div>
+      <h1>💸 Finance Tracker</h1>
+      <TransactionsList />
+    </div>
+  );
+}
+
+export default App;
